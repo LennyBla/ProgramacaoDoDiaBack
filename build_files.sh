7191a6dd1ed3a3be8298c4773bd2e28fa65d4561
LennyBla <thais.florentin26@gmail.com>
2024-05-20 16:52:22 -0300

    teste3

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,4 @@
+curl -sSL https://raw.githubusercontent.com/pypa/get-pip/master/get-pip.py | python -
+python -m pip install --upgrade pip
 pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
