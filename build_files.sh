b272ac547688c887c7a0322b451b1d181d7fe08d
LennyBla <thais.florentin26@gmail.com>
2024-05-20 17:00:16 -0300

    teste4

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+
+# Instala pip
 curl -sSL https://raw.githubusercontent.com/pypa/get-pip/master/get-pip.py | python -
+
+# Atualiza pip
 python -m pip install --upgrade pip
+
+# Instala dependências
 pip install -r requirements.txt
+
+# Coleta arquivos estáticos
 python3.9 manage.py collectstatic --noinput
