cc6d060de691ac9506f7ac18e85f7cae26f4b5fa
Thais <84157862+LennyBla@users.noreply.github.com>
2024-05-22 22:59:51 -0300

    22:44

--- setup/settings.py
+++ setup/settings.py
@@ -137,7 +137,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "http://localhost:3000", 
+    #https://programacaodo-dia.vercel.app/
+    
 ]
 
 STATIC_URL = '/static/'
