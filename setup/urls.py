d417d18b528fb5304fa00ccf880a8898b5dce6eb
Thais <84157862+LennyBla@users.noreply.github.com>
2024-05-22 22:05:50 -0300

    22:05

--- setup/urls.py
+++ setup/urls.py
@@ -42,3 +42,4 @@ urlpatterns = [
    path('api/v1/recreacao/<int:pk>/card/', ListaCardsDeUmRecreacaoView.as_view(), name='list_recreacao_card'),
    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
