f7932bff222ba6ddf3f5951b9f9b4ebe6d4e3c7e
LennyBla <thais.florentin26@gmail.com>
2024-05-24 16:19:20 -0300

    atualizacao do settings.py e build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,1 +1,5 @@
-python3.9 -m ensurepip python3.9 -m pip install -r requirements.txt python3.9 manage.py collectstatic --noinput --clear 
+python3.9 -m ensurepip 
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear 
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
