0b85599388e7f1160e3cd5572a8433f71a336d7a
LennyBla <thais.florentin26@gmail.com>
2024-05-27 16:36:10 -0300

    atualizacao do CRROS

--- setup/settings.py
+++ setup/settings.py
@@ -148,8 +148,9 @@ REST_FRAMEWORK = {
 
 # Django CORS Headers
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
+    #"http://localhost:3000",
+    "https://programacaodo-dia.vercel.app",
+ ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
